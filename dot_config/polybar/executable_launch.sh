696f8a576fbe2549ca4f99e2c76aa8bca335ad1c
isther <28251536@qq.com>
2022-07-01 21:13:32 +0800

    update

--- dot_config/polybar/executable_launch.sh
+++ dot_config/polybar/executable_launch.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+function ping()
+{
+	local timeout=1
+	local target=www.baidu.com
+	local ret_code=`curl -I -s --connect-timeout ${timeout} ${target} -w %{http_code} | tail -n1`
+
+	if [ "x$ret_code" = "x200" ]; then
+		return 1
+	else
+		return 0
+	fi
+	return 0
+}
+
 # Terminate already running bar instances
 killall -q polybar
 
@@ -7,8 +21,13 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 polybar -r bspwm &
-polybar -r network &
 polybar -r player &
 polybar -r other &
+polybar -r tray &
+
+ping
+if [ $? -eq 1 ];then
+	polybar -r network &
+fi
 
 echo "Polybar launched..."
